817624950c3f0f1ad02695acc92dc737373d07ae
kithmina1999 <124335388+kithmina1999@users.noreply.github.com>
2024-03-31 00:10:28 +0530

    home page almost completed

--- header.php
+++ header.php
@@ -11,6 +11,13 @@
     <!-- bootstrap cdn -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- fontawesome cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- fastbootstrap cdn -->
+    <link href="https://cdn.jsdelivr.net/npm/fastbootstrap@2.2.0/dist/css/fastbootstrap.min.css" rel="stylesheet" integrity="sha256-V6lu+OdYNKTKTsVFBuQsyIlDiRWiOmtC8VQ8Lzdm2i4=" crossorigin="anonymous">
+
 
 </head>
 
@@ -25,6 +32,7 @@
                         <div class="col-6">
                             <div class="row">
                                 <div class="d-flex gap-2 align-self-center p-2">
+                                <div class="offset-1 "></div>
                                     <?php
                                     session_start();
                                     if (isset($_SESSION["u"])) {
@@ -34,7 +42,7 @@
                                             Welcome!
                                             <?php echo $session_data["fname"] ?> <span class="text-gray">|</span>
                                         </div>
-                                        <div class="text-warning border border-warning px-1 pb-1 rounded-2" role="button">
+                                        <div class="text-warning fw-semibold  border border-warning px-1 pb-1 rounded-2" role="button">
                                             Sign Out </div>
                                         <div class="d-none d-lg-block">| Help and Contact</div>
                                     <?php } else {
@@ -43,7 +51,7 @@
                                             <a class="text-decoration-none text-danger" href="index.php">Log In or Sign
                                                 Up</a>
                                         </div>
-                                        <div class="d-none d-lg-block">| Help and Contact</div>
+                                        <div class="d-none d-lg-block  fw-bold ">| Help and Contact</div>
                                         <?php
                                     } ?>
                                 </div>
@@ -53,22 +61,39 @@
                         <!-- Right -->
                         <div class="col-6 ">
                             <div class="row">
-                                <div class="dropdown">
-                                    <button class="btn btn-secondary dropdown-toggle" type="button"
-                                        id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                        <i class="fa-solid fa-bars"></i>
-                                    </button>
-                                    <ul class="dropdown-menu">
-                                        <li><a class="dropdown-item" href="userProfile.php">My Profile</a></li>
-                                        <li><a class="dropdown-item" href="addProduct.php">Add New Product</a></li>
-                                        <li><a class="dropdown-item" href="#">My Sellings</a></li>
-                                        <li><a class="dropdown-item" href="myProducts.php">My Products</a></li>
-                                        <li><a class="dropdown-item" href="watchlist.php">Watchlist</a></li>
-                                        <li><a class="dropdown-item" href="#">Purchased History</a></li>
-                                        <li><a class="dropdown-item" href="#">Messages</a></li>
-                                        <li><a class="dropdown-item" href="#">Contact Admin</a></li>
-                                    </ul>
+                                <div class="d-flex justify-content-end  gap-8  align-items-center  ">
+                                    <div class="gap-2">
+                                    <i class="fa-solid fa-cart-shopping fs-5" role="button"></i>
+                                    </div>
+                                    <div class="dropdown mt-1">
+                                        <button class="btn btn-secondary " type="button" id="dropdownMenuButton1"
+                                            data-bs-toggle="dropdown" aria-expanded="false">
+                                            <i class="fa-solid fa-bars"></i>
+                                        </button>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="userProfile.php">My Profile</a></li>
+                                            <li><a class="dropdown-item" href="addProduct.php">Add New Product</a></li>
+                                            <li><a class="dropdown-item" href="#">My Sellings</a></li>
+                                            <li><a class="dropdown-item" href="myProducts.php">My Products</a></li>
+                                            <li><a class="dropdown-item" href="watchlist.php">Watchlist</a></li>
+                                            <li><a class="dropdown-item" href="#">Purchased History</a></li>
+                                            <li><a class="dropdown-item" href="#">Messages</a></li>
+                                            <li><a class="dropdown-item" href="#">Contact Admin</a></li>
+                                        </ul>
+                                    </div>
+                                    <?php 
+                                    if (isset($_SESSION["u"])) {
+                                    ?>
+                                    <div>
+                                        <img src="./resources/avatar.jpg" width="20px" class="avatar" />
+                                    </div>
+                                    <?php }else{
+                                        ?><?php
+                                    } ?>
+                                    <div class="offset-1 "></div>
+
                                 </div>
+
                             </div>
 
 
@@ -84,7 +109,7 @@
 
 
 
-
+                                    
     <script src="script.js" async defer></script>
 </body>
 
--- home.php
+++ home.php
@@ -1,25 +1,209 @@
 <!DOCTYPE html>
 <?php include "connection.php"; ?>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="style.css">
-        <!-- bootstrap cdn -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    </head>
-    <body>
-       <div>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+    <!-- bootstrap cdn -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- fastbootstrap cdn -->
+    <link href="https://cdn.jsdelivr.net/npm/fastbootstrap@2.2.0/dist/css/fastbootstrap.min.css" rel="stylesheet"
+        integrity="sha256-V6lu+OdYNKTKTsVFBuQsyIlDiRWiOmtC8VQ8Lzdm2i4=" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div>
         <?php include "header.php" ?>
-       </div>
+    </div>
+    <hr />
+
+    <div class="col-12 justify-content-center">
+        <div class="row mb-3">
+
+            <div class="offset-4 offset-lg-1 col-4 col-lg-1 logo" style="height: 60px;"></div>
+
+            <div class="col-12 col-lg-6">
+
+                <div class="input-group mb-3 mt-3">
+                    <input type="text" id="kw" class="form-control" aria-label="Text input with dropdown button" />
+
+                    <select class="form-select" style="max-width: 250px;" id="c">
+                        <option value="0">All Categories</option>
+
+                        <?php
+
+                        $category_rs = Database::search("SELECT * FROM `category`");
+                        $category_num = $category_rs->num_rows;
+
+                        for ($x = 0; $x < $category_num; $x++) {
+
+                            $category_data = $category_rs->fetch_assoc();
+
+                            ?>
+
+                            <option value="<?php echo $category_data["cat_id"]; ?>">
+                                <?php echo $category_data["cat_name"]; ?>
+                            </option>
+
+                            <?php
+
+                        }
+
+                        ?>
+
+                    </select>
+
+                </div>
+
+            </div>
+
+            <div class="col-12 col-lg-2 d-grid">
+                <button class="btn btn-danger mt-3 mb-3" onclick="basicSearch(0);">Search</button>
+            </div>
+
+            <div class="col-12 col-lg-2 mt-2 mt-lg-4 text-center text-lg-start">
+                <a href="advancedSearch.php" class="text-decoration-none link-secondary fw-bold">Advanced</a>
+            </div>
+
+        </div>
+    </div>
+
+    <hr />
+    <div class="col-12">
+        <!-- Carousel -->
+        <div class="row">
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100 " src="./resources/carousel-1.jpg" alt="carousel-1.jpeg" />
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Tokyo (Japan)</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100 " src="./resources/carousel-2.jpg" alt="carousel-2.jpeg" />
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Tokyo (Japan)</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="./resources/carousel-3.jpg" alt="carousel-3.jpeg" />
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Tokyo (Japan)</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <?php
+    $rs = Database::search("SELECT * FROM `category` ");
+    $cn = $rs->num_rows;
+
+    for ($x = 0; $x < $cn; $x++) {
+        $cd = $rs->fetch_assoc();
+        ?>
+        <div class="col-12 ">
+            <!-- Category -->
+            <div class="row mb-3">
+                <div class="d-flex justify-content-start p-2 gap-4 align-items-center ms-5">
+                    <a href="#" class="text-dark fs-5 fw-bold text-decoration-none">
+                        <?php echo $cd["cat_name"]; ?>
+                    </a>
+                    <a href="#" class="text-danger fw-bold text-decoration-none">See all</a>
+                </div>
+            </div>
+            <!-- Products -->
+            <div class="row mb-3">
+                <div class="col-12">
+                    <div class="row border border-dark m-1">
+                        <div class="col-12 p-1">
+                            <div class="row justify-content-center gap-4">
+                                <!-- Product Card -->
+                                <?php
+                                $pr = Database::search("SELECT * FROM `product` WHERE `category_cat_id`='" . $cd["cat_id"] . "' AND
+                            `status_status_id`='1' ORDER BY  `datatime_added` DESC LIMIT 4 OFFSET 0");
+                                $pn = $pr->num_rows;
+
+                                for ($x = 0; $x < $pn; $x++) {
+                                    $product_data = $pr->fetch_assoc();
+                                    ?>
+
+                                    <div class="card col-12 col-lg-2 col-md-4">
+                                        <?php
+                                        $ir=Database::search("SELECT * FROM `product_img` WHERE `product_id`='".$product_data["id"]."'");
+                                        $image_data=$ir->fetch_assoc();
+                                        ?>
+                                        
+                                        <img src="<?php echo $image_data["img_path"] ?>" class="<?php echo $product_data["title"] ?>"
+                                            alt="green iguana" />
+                                        <div class="card-body">
+                                            <h4 class="text-center fw-bold ">
+                                                <?php echo $product_data["title"] ?>
+                                            </h4>
+                                            <div class="d-flex flex-column  justify-content-center">
+                                                <p class=""><span class="fw-bold">Items Available: </span>
+                                                    <?php echo $product_data["qty"] ?>
+                                                </p>
+                                                <p class=""><span class="fw-bold">LKR: </span>
+                                                    <?php echo $product_data["price"] ?>
+                                                </p>
+                                            </div>
+
+                                            <div>
+                                                <a href="<?php echo "singleProductView.php?id=" . ($product_data["id"]); ?>"
+                                                    class="col-12 btn btn-success">Buy Now</a>
+                                                <button class="col-12 btn btn-dark mt-2"
+                                                    onclick="addToCart(<?php echo $product_data['id']; ?>);">
+                                                    <i class="fa-solid fa-cart-shopping text-white fs-5"></i>
+                                                </button>
+                                                <button onclick="addToWatchlist(<?php echo $product_data['id']; ?>);"
+                                                    class="col-12 btn btn-outline-light mt-2 border border-primary">
+                                                    <i class="fa-solid fa-heart text-dark fs-5"></i>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <?php
+                                }
+
+                                ?>
+
+
+                                <!-- Product Card -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php
+    }
+
+    ?>
 
 
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-        <script src="script.js" async defer></script>
-    </body>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+    <script src="script.js" async defer></script>
+</body>
+
 </html>
